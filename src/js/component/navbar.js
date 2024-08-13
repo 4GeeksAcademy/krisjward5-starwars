c89fe4644ab14e46c05ecc5762352a739758e1d7
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-08-13 01:05:37 +0000

    working

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,29 +1,42 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { FaTrash } from "react-icons/fa";
-import { Context } from "../store/appContext"
-
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	const { store, actions } = useContext{ Context };
+	const { store, actions } = useContext(Context);
 
 	return (
-		<nav className="navbar navbar-light bg-light mb3">
-			<div class="dropdown">
-				<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		<nav className="navbar navbar-light bg-light mb-3">
+			<div className="dropdown">
+				<button
+					className="btn btn-secondary dropdown-toggle"
+					type="button"
+					id="dropdownMenuButton"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false"
+				>
 					Favorites {store.favorites.length}
 				</button>
-				<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-					{store.favorites.map((favorite) => (
-						<li>
-							<span>{favorite.nam}</span>
-							<FaTrash onClick={() => actions.toggleFavorite(
-								favorite.id,
-								favorite.type,
-								favorite.name,)}/>
-						</li>
-					))}
-				</ul>
+				<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+					<ul>
+						{store.favorites.map((favorite) => (
+							<li key={favorite.id}>
+								<span>{favorite.name}</span>
+								<FaTrash
+									onClick={() =>
+										actions.toggleFavorite(
+											favorite.id,
+											favorite.type,
+											favorite.name
+										)
+									}
+								/>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
