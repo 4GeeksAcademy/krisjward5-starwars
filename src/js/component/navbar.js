0f77fd6e94b05f0c177fa3c1379dc8d7aeafb898
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-08-13 00:59:33 +0000

    working

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+import { Context } from "../store/appContext"
+
 
 export const Navbar = () => {
+	const { store, actions } = useContext{ Context };
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-light bg-light mb3">
+			<div class="dropdown">
+				<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Favorites {store.favorites.length}
+				</button>
+				<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+					{store.favorites.map((favorite) => (
+						<li>
+							<span>{favorite.nam}</span>
+							<FaTrash onClick={() => actions.toggleFavorite(
+								favorite.id,
+								favorite.type,
+								favorite.name,)}/>
+						</li>
+					))}
+				</ul>
 			</div>
 		</nav>
 	);
