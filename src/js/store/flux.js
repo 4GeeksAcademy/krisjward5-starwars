23716d76ea3b740be8835313899c6dfb508b2174
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-08-13 00:20:04 +0000

    working

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,19 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			fetchCharacters: () => {
+				fetch('https://www.swapi.tech/api/people?limit=100')
+					.then(res => res.json())
+					.then((payload) => {
+						setStore({ characters: payload.results })
+					})
+					.catch((err) => console.error(err))
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
-};
-
-export default getState;
+}
+export default getState
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
 import "../../styles/home.css";
+import {Context} from "../store/appContext.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () => {
+	const { store, actions } = useContext(Context)
+
+useEffect(() => {
+
+	actions.fetchCharacters();
+}, []);
+
+return (
+
+<div className="text-center mt-5">
+	<p>home page</p>
+	<div>
+		{store.characters.map((character) => (
+			<div>
+				<p>{character.name}</p>
+			</div>
+		))}
 	</div>
-);
+	</div>
+
+	)
+};
